3bb4702b906bc555dd55882c121af2915fbdc551
Daniel Francisco <daniel_mp18@outlook.com>
2021-09-17 12:51:21 -0300

    feat:feature add card

---
+++ src/components/Form/index.jsx
@@ -0,0 +1,87 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { makeStyles } from "@material-ui/core/styles";
+import { Button, Grid } from "@material-ui/core";
+import logo_github from "./logo-github.png";
+import Search from "../Search";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: "100vh",
+    color: "#F0F0F0",
+  },
+  input: {
+    width: "250px",
+    marginBottom: "5vh",
+    height: "4vh",
+  },
+  form: {
+    marginTop: "2vh",
+    textAlign: "center",
+    justifyContent: "center",
+  },
+  button: {
+    height: "5vh",
+    borderRadius: "10px",
+  },
+  img: {
+    textAlign: "center",
+    width: "300px",
+  },
+}));
+const Form = () => {
+  const { handleSubmit } = useForm({});
+  const [search, setSearch] = useState("");
+  const [istrue, setIsTrue] = useState(false);
+  const [repo, setRepo] = useState("");
+  const classe = useStyles();
+  const onHandleSubmit = () => {
+    setRepo(search);
+    setIsTrue(true);
+  };
+
+  return (
+    <>
+      <Grid
+        container
+        justifyContent="center"
+        direction="row"
+        alignItems="center"
+        className={classe.root}
+      >
+        {istrue ? (
+          <div>
+            <Search repo={repo} setIstrue={setIsTrue} setSearch={setSearch} />
+          </div>
+        ) : (
+          <Grid item>
+            <div className={classe.logo}></div>
+            <img className={classe.img} src={logo_github} alt="github" />
+            <form
+              className={classe.form}
+              onSubmit={handleSubmit(onHandleSubmit)}
+            >
+              <input
+                placeholder="Digite o repositorio que deseja acessar"
+                className={classe.input}
+                type="text"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+
+              <Button
+                color={"primary"}
+                className={classe.button}
+                variant={"contained"}
+                type="submit"
+              >
+                Pesquisar
+              </Button>
+            </form>
+          </Grid>
+        )}
+      </Grid>
+    </>
+  );
+};
+export default Form;
---
+++ src/components/Search/index.jsx
@@ -0,0 +1,80 @@
+import { useEffect } from "react";
+import { useState } from "react/cjs/react.development";
+import { makeStyles } from "@material-ui/core/styles";
+import { Button, Grid, Paper } from "@material-ui/core";
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: "100vh",
+    color: "#F0F0F0",
+  },
+  button: {
+    color: "#F0F0F0",
+    marginTop: "5vh",
+    padding: "10px",
+  },
+  paper: {
+    backgroundColor: "black",
+    color: "#F0F0F0",
+    padding: "10px",
+    textAlign: "center",
+  },
+  img: {
+    width: "50px",
+  },
+}));
+const Search = ({ repo, setIstrue, setSearch }) => {
+  useEffect(() => {
+    fetch(`https://api.github.com/repos/${repo}`)
+      .then((Response) => Response.json())
+      .then((Response) => setResult(Response));
+  }, [repo]);
+  const handleClick = () => {
+    setIstrue(false);
+    setSearch("");
+  };
+  const classe = useStyles();
+  const [result, setResult] = useState("");
+  console.log(result);
+  return (
+    <>
+      {result.id ? (
+        <Grid
+          className={classe.root}
+          container
+          justifyContent="center"
+          alignItems="center"
+          direction="column"
+        >
+          <Grid item justifyContent="center" xs={6}>
+            <Paper className={classe.paper}>
+              <img
+                className={classe.img}
+                src={result.owner.avatar_url}
+                alt="avatar"
+              />
+              <h3>{result.full_name}</h3>
+              <p>{result.description}</p>
+              <details>
+                <a href={result.svn_url}>Clique aqui</a>
+                <summary>
+                  Para ter acesso ao repositorio do {result.name}
+                </summary>
+              </details>
+              <Button
+                variant={"contained"}
+                color={"primary"}
+                onClick={() => handleClick()}
+                className={classe.button}
+              >
+                Pesquisar novo repositorio
+              </Button>
+            </Paper>
+          </Grid>
+        </Grid>
+      ) : (
+        <div>{result.message}</div>
+      )}
+    </>
+  );
+};
+export default Search;
