77c94ea1931fc054512fef2b40bc74867cc4e57d
Daniel Francisco <daniel_mp18@outlook.com>
2021-09-17 13:11:12 -0300

    fix bugs

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -26,6 +26,7 @@ const useStyles = makeStyles((theme) => ({
     color: "black",
   },
 }));
+//
 const Search = ({ repo, setIstrue, setSearch }) => {
   useEffect(() => {
     fetch(`https://api.github.com/repos/${repo}`)
